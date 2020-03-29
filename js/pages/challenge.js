c407b3b815ce50dcf3aafc9068893b55764a48ac
JeffD-dev <dev@zest-labs.fr>
2020-03-29 22:02:29 +0200

    feat: add page challenge and reorganize page tasks

---
+++ js/pages/challenge.js
@@ -0,0 +1,5 @@
+var Pages = Pages || {}
+Pages.tasks=function() {
+  $main.html($('#challenge').html());
+  $body.attr('class', 'page-challenge bg-linear-gradient');
+};
--- js/pages/tasks.js
+++ js/pages/tasks.js
@@ -1,6 +1,5 @@
 var Pages = Pages || {}
 Pages.tasks=function() {
   $main.html($('#tasks').html());
-  $body.attr('class', 'page-tasks');
-  $body.attr('class', 'page-capacity bg-linear-gradient');
+  $body.attr('class', 'page-tasks bg-linear-gradient');
 };
