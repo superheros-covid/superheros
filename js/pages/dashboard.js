f8e09cb7236641a6f10679b3691266c4775b3515
JeffD-dev <dev@zest-labs.fr>
2020-03-30 01:38:30 +0200

    feat: add indication about life sauved on dashboard

--- js/pages/dashboard.js
+++ js/pages/dashboard.js
@@ -13,4 +13,20 @@ Pages.dashboard=function() {
   if(localStorage.getItem('hasTask') == 1) {
     $main.find('#btn-tasks').show();
   }
+
+  var save = 0;
+  switch(team){
+    case "blue": save=6.4; break;
+    case "orange": save=0.6; break;
+    case "pink": save=0.2; break;
+    case "yellow": save=0.8; break;
+  }
+  localStorage.setItem('savePeople', save);
+  if(save>2){
+    message = "Si vous restez chez vous aujourd'hui, vous contribuerez à sauver "+ Math.round(save)+" vies.";
+  } else {
+    message = "Si vous restez chez vous aujourd'hui, vous contribuerez à sauver une vie";
+  }
+  $main.find('#life-counter').append(message);
+
 };
