e6ec9a565003fd65e0b3af8d3b9f32ef37ae92b7
JeffD-dev <dev@zest-labs.fr>
2020-04-01 21:43:22 +0200

    fix: change validateTask function scope and fill task when selected in task selection page

--- js/pages/challenge.js
+++ js/pages/challenge.js
@@ -5,6 +5,15 @@ Pages.challenge=function() {
   var team = teamNeeded();
   $main.find('.colored-roue').attr('src', '/img/colors_decline/'+team+'/roue.png');
 
+  taskIds = ["one", "two", "three"];
+  for(let i=0;i<taskIds.length; i++){
+    id = taskIds[i];
+    var storedTask = localStorage.getItem('task'+id);
+    if (storedTask.length){
+      $('input[name=task'+id+']').val(storedTask)
+    }
+  }
+
   $('#roue').on('click', function() {
     taskIds = ["one", "two", "three"];
     for(let i=0;i<taskIds.length; i++){
@@ -17,28 +26,28 @@ Pages.challenge=function() {
     }
   });
 
-  function getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-  function validateTask(id){
-    var taskInput = $('input[name=task'+id+']').val();
-    if(taskInput.length) {
-      localStorage.setItem('task'+id, taskInput);
-
-      tasksCounter = parseInt(localStorage.getItem('hasTask') || 0);
-      tasksCounter += 1;
-      localStorage.setItem('hasTask', tasksCounter);
-
-      if(tasksCounter == 0) {
-        document.location.hash='challenge';
-      }
-    }
-  }
-
   function getRandomActivity(){
     activities = ["Regardez un film/épisode de série", "Écoutez un podcast", "Débutez un cours en ligne (MOOC)", "Lisez le chapitre d’un livre", "Raconter à l’écrit votre confinement", "Faites 10 minutes de méditation", "Téléphoner à un proche", "Faites un peu de repassage", "Nettoyez les vitres", "Passez l’aspirateur", "Faites une séance de fitness/yoga", "Réfléchissez à vos futurs projets", "Triez et rangez un placard", "Faites quelques photos de votre quotidien", "Faites un peu de couture", "Dessinez ou faites de la peinture", "Grand nettoyage de printemps", "Démarrez un projet personnel", "Créez un album de vos meilleurs photos", "Triez vos vetements"];
     randomId = getRandomArbitrary(0, activities.length);
     return activities[randomId];
   }
+
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 };
+
+function validateTask(id){
+  var taskInput = $('input[name=task'+id+']').val();
+  if(taskInput.length) {
+    localStorage.setItem('task'+id, taskInput);
+
+    tasksCounter = parseInt(localStorage.getItem('hasTask') || 0);
+    tasksCounter += 1;
+    localStorage.setItem('hasTask', tasksCounter);
+
+    if(tasksCounter == 0) {
+      document.location.hash='challenge';
+    }
+  }
+}
