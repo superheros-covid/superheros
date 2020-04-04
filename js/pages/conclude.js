38ec258cff003b85db58216b0a9fd53a770ed524
JeffD-dev <dev@zest-labs.fr>
2020-04-04 22:55:51 +0200

    fix: invalid css for btn-blue

--- js/pages/conclude.js
+++ js/pages/conclude.js
@@ -3,6 +3,7 @@ Pages.conclude=function() {
   $body.attr('class', 'bg-linear-gradient');
   $main.html($('#conclude').html());
 
+  var team = teamNeeded();
   $main.find('.colored-cmask').attr('src', '/img/colors_decline/'+team+'/cmask.png');
   $main.find('.colored-virus').attr('src', '/img/colors_decline/'+team+'/virus.png');
   $main.find('.colored-trophy').attr('src', '/img/colors_decline/'+team+'/trophy.png');
