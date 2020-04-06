e7572365f479653e663e37df842590f14438dbd3
JeffD-dev <dev@zest-labs.fr>
2020-04-06 23:11:38 +0200

    feat: save trophy (lives saved) progression

--- js/pages/conclude.js
+++ js/pages/conclude.js
@@ -8,4 +8,24 @@ Pages.conclude=function() {
   $main.find('.colored-virus').attr('src', '/img/colors_decline/'+team+'/virus.png');
   $main.find('.colored-trophy').attr('src', '/img/colors_decline/'+team+'/trophy.png');
   $main.find('.colored-number').attr('class', 'text-team-'+team);
+
+  $main.find('.colored-number').attr('class', 'text-team-'+team);
+
+  /* Increment life counter */
+  var savePeople = parseFloat(localStorage.getItem('savePeople') || 0);
+  var displaySave = 1;
+  if(savePeople>1){
+    displaySave = Math.round(savePeople);
+  }
+  $main.find('#lifeCount').text(Math.round(displaySave));
+
+  var dateLastTrophy = localStorage.getItem('lastTrophy');
+  var today = new Intl.DateTimeFormat('en-US').format(new Date());
+
+  if (dateLastTrophy != today){
+    var lifes = parseFloat(localStorage.getItem('lifes') || 0);
+    lifes += savePeople;
+    localStorage.setItem('lifes', lifes);
+    localStorage.setItem('lastTrophy', today);
+  }
 };
