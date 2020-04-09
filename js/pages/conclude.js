d424da6cea545f5479445ccb73879ba28f363f6c
JeffD-dev <dev@zest-labs.fr>
2020-04-09 10:31:59 +0200

    feat: add new trophy message on conclude page

--- js/pages/conclude.js
+++ js/pages/conclude.js
@@ -14,18 +14,31 @@ Pages.conclude=function() {
   /* Increment life counter */
   var savePeople = parseFloat(localStorage.getItem('savePeople') || 0);
   var displaySave = 1;
+
+  levels = [3, 5, 10, 15, 20, 25, 30, 40, 50, 75, 100, 150, 200];
   if(savePeople>1){
     displaySave = Math.round(savePeople);
   }
   $main.find('#lifeCount').text(Math.round(displaySave));
 
-  var dateLastTrophy = localStorage.getItem('lastTrophy');
+  var datelastConclude = localStorage.getItem('lastConclude');
   var today = new Intl.DateTimeFormat('en-US').format(new Date());
 
-  if (dateLastTrophy != today){
-    var lifes = parseFloat(localStorage.getItem('lifes') || 0);
-    lifes += savePeople;
-    localStorage.setItem('lifes', lifes);
-    localStorage.setItem('lastTrophy', today);
+  if (datelastConclude != today){
+    var oldLifes = parseFloat(localStorage.getItem('lifes') || 0);
+    newLifes = oldLifes + savePeople;
+    localStorage.setItem('lifes', newLifes);
+    localStorage.setItem('lastConclude', today);
+  }
+
+  oldLevel = 0;
+  newLevel = 0;
+  for(i=0;i<this.levels.length;i++){
+    if(levels[i] <= oldLifes){oldLevel = levels[i];}
+    if(levels[i] <= newLifes){newLevel = levels[i];}
+    if(levels[i] > newLifes){break;}
+  }
+  if(newLevel > oldLevel) {
+    $main.find('#new-trophy').append("Félicitation! Vous avez obtenu un nouveau trophé! ");
   }
 };
---
+++ js/pages/trophy.js
@@ -0,0 +1,5 @@
+var Pages = Pages || {}
+Pages.trophy=function() {
+  $body.attr('class', 'bg-linear-gradient');
+  $main.html($('#trophy').html());
+};
